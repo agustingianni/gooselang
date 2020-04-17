ca7c8c8325c9f5ec097f919cc5cacb45f745ae48
Agustin Gianni <agustingianni@gmail.com>
2020-04-17 15:14:40 +0200

    Adds an interpreter project

---
+++ src/Interpreter/Library.fs
@@ -0,0 +1,5 @@
+﻿namespace Interpreter
+
+module Say =
+    let hello name =
+        printfn "Hello %s" name
---
+++ src/Parser/Parser.fs
@@ -0,0 +1,22 @@
+module Parser
+
+open FSharp.Text.Lexing
+
+let parseString (input: string) =
+    let tokenized = LexBuffer<char>.FromString(input)
+
+    try
+        YaccParser.program Lexer.tokenize tokenized
+
+    with e ->
+        let pos = tokenized.EndPos
+        let line = pos.Line
+        let column = pos.Column
+        let lastToken = System.String(tokenized.Lexeme)
+        eprintfn "Parse failed at line %d, column %d" line column
+        eprintfn "Input: %A" input
+        exit 1
+
+let parseFile (path: string) =
+    let input = System.IO.File.ReadAllText(path)
+    parseString input

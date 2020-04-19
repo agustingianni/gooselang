036ddc283cb18cb539ca1546d7097ea9117ac250
Agustin Gianni <agustingianni@gmail.com>
2020-04-19 16:58:29 +0200

    Removes development commets from the AST

--- src/Parser/Ast.fs
+++ src/Parser/Ast.fs
@@ -34,9 +34,9 @@ type Literal =
     | Float of float
     | Float32 of float32
     | String of string
+    | Unit
 
 type Expression =
-    | Unit
     | Constant of Literal
     | Variable of Name
     | PrefixExpression of string * Expression
@@ -52,92 +52,9 @@ type Expression =
     | Union of UnionField list
     | While of Expression * Expression
 
-    // | Let of Expression * Expression
-    // | Match
-    // | For
-
 type Declaration =
     | Open of Name
     | Let of Name * Expression
 
 type Program =
-    Program of Declaration list * Expression
-
-type SynExpr = unit
-
-type Ident(text: string) =
-    member x.idText = text
-    override x.ToString() = text
-
-type LongIdent = Ident list
-
-type SynModuleOrNamespaceKind =
-    | NamedModule
-    | AnonModule
-
-type range = unit
-
-type SynAccess =
-    | Public
-    | Internal
-    | Private
-
-type SynBindingKind =
-
-    /// A standalone expression in a module
-    | StandaloneExpression
-
-    /// A normal 'let' binding in a module
-    | NormalBinding
-
-    /// A 'do' binding in a module. Must have type 'unit'
-    | DoBinding
-
-type TypeName = Name
-
-type SynAttribute =
-    { TypeName: LongIdent
-
-      ArgExpr: SynExpr
-
-      /// Target specifier, e.g. "assembly", "module", etc.
-      Target: Ident option
-
-      /// Is this attribute being applied to a property getter or setter?
-      AppliesToGetterAndSetter: bool
-
-      Range: range }
-
-type SynAttributeList =
-    { Attributes: SynAttribute list
-      Range: range }
-
-type SynAttributes = SynAttributeList list
-
-type SynBinding =
-    Binding of accessibility: SynAccess option *
-    kind: SynBindingKind *
-    attrs: SynAttributes *
-    expr: SynExpr
-
-type SynTypeDefn = TypeDefn
-
-type SynModuleDecl =
-    | Let of isRecursive: bool * SynBinding list * range: range
-    | Types of SynTypeDefn list * range: range
-    | Open of longDotId: LongIdent * range: range
-
-type SynModuleDecls = SynModuleDecl list
-
-type SynModuleOrNamespace =
-    SynModuleOrNamespace of longId: LongIdent *
-    kind: SynModuleOrNamespaceKind *
-    decls: SynModuleDecls *
-    attribs: SynAttributes *
-    accessibility: SynAccess option
-
-type ParsedImplFileFragment =
-    | AnonModule of SynModuleDecls
-    | NamedModule of SynModuleOrNamespace
-
-type ParsedImplFile = ParsedImplFile of ParsedImplFileFragment list
+    Program of Declaration list * Expression option

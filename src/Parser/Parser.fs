4c72f52f4eabfe57e2d9199bfd88f22d4ff1df8a
Agustin Gianni <agustingianni@gmail.com>
2020-04-19 23:23:44 +0200

    Adds a way to get a list of tokens

--- src/Parser/Parser.fs
+++ src/Parser/Parser.fs
@@ -2,17 +2,27 @@ module GooseLangParser
 
 open FSharp.Text.Lexing
 
+let parseTokens (input: string) =
+    let buffer = LexBuffer<char>.FromString input
+    seq {
+        let mutable stop = false
+        while not stop do
+            let token = Lexer.tokenize buffer
+            if token = Parser.EOF then stop <- true else yield token
+    }
+    |> List.ofSeq
+
 let parseString (input: string) =
-    let tokenized = LexBuffer<char>.FromString(input)
+    let buffer = LexBuffer<char>.FromString(input)
 
     try
-        Parser.program Lexer.tokenize tokenized
+        Parser.program Lexer.tokenize buffer
 
     with e ->
-        let pos = tokenized.EndPos
+        let pos = buffer.EndPos
         let line = pos.Line
         let column = pos.Column
-        let lastToken = System.String(tokenized.Lexeme)
+        let lastToken = System.String(buffer.Lexeme)
         eprintfn "Parse failed at line %d, column %d" line column
         eprintfn "Input: %A" input
         exit 1
